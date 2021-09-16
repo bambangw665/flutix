b6ff99fcb014bdf92c263e2eec6da376b893ad89
bambangw665 <bambangw665@pop-os.localdomain>
2021-09-16 14:03:12 +0700

    user services

---
+++ lib/models/user.dart
@@ -0,0 +1,26 @@
+part of 'models.dart';
+
+class User extends Equatable {
+  final String id;
+  final String email;
+  final String profilePicture;
+  final String name;
+  final List<String> selectdGenres;
+  final String selectedLanguage;
+  final int balance;
+
+  User(this.id, this.email, this.profilePicture, this.name, this.selectdGenres,
+      this.selectedLanguage, this.balance);
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [
+        id,
+        email,
+        name,
+        profilePicture,
+        selectdGenres,
+        selectedLanguage,
+        balance,
+      ];
+}
---
+++ lib/shared/shared_value.dart
@@ -0,0 +1,4 @@
+part of 'shared.dart';
+
+String apiKey = "f2ce1c5c29fea5e7243ff8ae56b24f8e";
+String imageBaseURL = "https://image.tmdb.org/t/p/";
